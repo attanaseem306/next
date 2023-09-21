5c5f9361b6e26222151ca7b771fd506b432d5ae2
MRMutahir <mutahirkareem820@example.com>
2023-09-21 20:44:40 +0500

    nextfirst

---
+++ public/Data/page.js
@@ -0,0 +1,32 @@
+
+ export  const Data=[
+   
+    {
+        Name:'Muhammad Atta',
+        class:'2nd Year',
+        Roll:10194,
+        url:'https://media.istockphoto.com/id/1393481374/photo/portrait-of-the-boy-on-avenida-paulista.jpg?s=1024x1024&w=is&k=20&c=SVOrnwVhfkYSzlwR5Wfir0LGSqh2pn88WtJucIP2wCg='
+    }
+     ,
+    {
+        Name:'Muhammad Junaid',
+        class:'1st Year',
+        Roll:10043,
+        url:'https://media.istockphoto.com/id/1517853305/photo/selfie-portrait-looking-at-camera-of-asian-teenage-boy-making-peace-sign-happy-people-positive.jpg?s=1024x1024&w=is&k=20&c=r24nBF1Msdht4N5pmFjZl-oWyqL8v8rXZzvKN7tGkDY='
+    }
+     ,
+    {
+        Name:'Muhammad Faiz',
+        class:'3rd Year',
+        Roll:10331,
+        url:'https://media.istockphoto.com/id/1359234676/photo/one-caucasian-boy-lying-on-the-floor-at-home-in-day-reading-a-book-front-view-copy-space-real.jpg?s=1024x1024&w=is&k=20&c=Pjrn_RVQqtpUpdQxWYKPx6sCLO7UGpSYNubnAapRdDI='
+    }
+     ,
+    {
+        Name:'Muhammad Taha',
+        class:'4th Year',
+        Roll:203194,
+        url:'https://media.istockphoto.com/id/1500076632/photo/happy-black-student-in-high-school-looking-at-camera.jpg?s=1024x1024&w=is&k=20&c=YvKPw_3v9mMQ73m_m-0SkN01q14w8OmgfXYyTy7pXHg='
+    }
+     
+]
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,43 @@
+'use client'
 import Image from 'next/image'
+import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+import Link from 'next/link'
+import { Data } from '../../public/Data/page';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+
+
+// export default ListGroupExample;
 
 export default function Home() {
+  console.log(Data);
   return (
    <>
-   <h1>Hello Next</h1>
+   <div style={{ paddingLeft:15,paddingRight:15 ,display:'inline-flex', display:'flex', justifyContent:'space-evenly',flexWrap:'wrap'}}>
+    {
+      Data.map((e)=>{
+        return (
+          <div className='main' style={{marginTop:20}}>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={e.url} />
+                <Card.Body>
+                  <Card.Title><h3 style={{fontFamily:'revert-layer'}}>{e.Name}</h3></Card.Title>
+                  <Card.Title>{e.class}</Card.Title>
+                  <Card.Title>{e.Roll}</Card.Title>
+
+                </Card.Body>
+              </Card> 
+              </div>
+        )
+      })
+    }
+   </div>
+   {/* <Link href={'/About'}> <button>About</button></Link>
+   <button>Contact Us</button>
+   <button>Blog</button> */}
    </>
   )
 }
