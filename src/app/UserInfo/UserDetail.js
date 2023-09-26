37b3044d2b76e7714a3922750423042a8a6e6961
MRMutahir <mutahirkareem820@example.com>
2023-09-26 19:33:59 +0500

    'Solve'

--- src/app/UserInfo/UserDetail.js
+++ src/app/UserInfo/UserDetail.js
@@ -1,13 +1,9 @@
 export default async function UserDetail(id) {
-    console.log(id);
-    
-    const rest=await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+
+    const rest = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
 
     if (!rest.ok) {
-        
-        throw new Error (" Failed to fetch data")
-
+        throw new Error(" Failed to fetch data")
     }
- 
-return rest.json()
+    return rest.json()
 }
